996def251df79c3750d42308d099ad75f7a70550
YuvathikaB <yuvathika2020@gmail.com>
2025-04-28 12:00:00 +0530

    Added programs of replace and modify problems on regex

---
+++ Day5/Regex/ReplaceModify/ReplaceSpace.java
@@ -0,0 +1,12 @@
+package replaceModify;
+import java.util.Scanner;
+public class ReplaceSpace {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("Enter text: ");
+       String input = scanner.nextLine();
+       String output = input.replaceAll("\\s+", " ");
+       System.out.println(output);
+   }
+}
+
